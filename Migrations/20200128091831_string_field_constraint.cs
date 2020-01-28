29f99fe2ed83ed51658edb36bb6ec7556310f4f5
Paul Milbank <paulmilbank@pop-os.localdomain>
2020-01-28 22:28:46 +1300

    Ensure the stored string is unique so we don't have to delete them all the time

---
+++ Migrations/20200128091831_string_field_constraint.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace db_app.Migrations
+{
+    public partial class string_field_constraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_StoredStrings_stringToStore",
+                table: "StoredStrings",
+                column: "stringToStore",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StoredStrings_stringToStore",
+                table: "StoredStrings");
+        }
+    }
+}
--- Models/StorageModel.cs
+++ Models/StorageModel.cs
@@ -9,6 +9,13 @@ namespace db_app.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseNpgsql("Server=localhost; Database=testdb; UserName=testuser; Password=testpassword;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StoredString>()
+                .HasIndex(s => s.stringToStore)
+                .IsUnique();
+        }
     }
 
     public class StoredString
